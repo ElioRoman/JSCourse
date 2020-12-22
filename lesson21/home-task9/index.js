a3956bf48027e5ced3b00b1bca53a807bdfa0fa7
ElioRoman <kolomiets.roman30@gmail.com>
2020-12-22 17:40:36 +0200

    add file

---
+++ lesson21/home-task9/index.js
@@ -0,0 +1,9 @@
+const finishForm = () => {
+  const elemList = document.querySelector('input');
+  elemList.setAttribute('type', 'text');
+  elemList.setAttribute('name', 'login');
+  const inputNew = document.createElement('input');
+  elemList.append(inputNew);
+  inputNew.setAttribute('type', 'password');
+  inputNew.setAttribute('name', 'password');
+};
