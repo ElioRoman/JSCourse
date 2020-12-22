2733030057c7bcd12aac0533e301a66c6d022370
ElioRoman <kolomiets.roman30@gmail.com>
2020-12-22 22:42:50 +0200

    fix

--- lesson21/home-task11/index.js
+++ lesson21/home-task11/index.js
@@ -1,5 +1,5 @@
 export const squaredNumbers = () => {
-  const a = document.querySelectorAll('.number');
-  [...a].map(el => (el.dataset.squaredNumber = el.dataset.number ** 2));
+  const getNum = document.querySelectorAll('.number');
+  [...getNum].map(el => (el.dataset.squaredNumber = el.dataset.number ** 2));
 };
 squaredNumbers();
