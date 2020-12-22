f828e25b00abb52ded538a75e2fab81201074351
ElioRoman <kolomiets.roman30@gmail.com>
2020-12-22 17:03:54 +0200

    add file

---
+++ lesson21/home-task8/index.js
@@ -0,0 +1,16 @@
+export const finishList = () => {
+  const elemLi1 = document.createElement('li');
+  const elemLi4 = document.createElement('li');
+  const elemLi6 = document.createElement('li');
+  const elemLi8 = document.createElement('li');
+  elemLi1.textContent = '1';
+  elemLi4.textContent = '4';
+  elemLi6.textContent = '6';
+  elemLi8.textContent = '8';
+  const elemList = document.querySelector('.list');
+  elemList.append(elemLi8);
+  elemList.prepend(elemLi1);
+  const elemSpec = document.querySelector('.special');
+  elemSpec.before(elemLi4);
+  elemSpec.after(elemLi6);
+};
