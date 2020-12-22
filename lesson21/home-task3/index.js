3f341efb2d13d5ecaaf1d6fbf3d4b0cdb09887f9
ElioRoman <kolomiets.roman30@gmail.com>
2020-12-22 15:24:08 +0200

    add file

---
+++ lesson21/home-task3/index.js
@@ -0,0 +1,19 @@
+export const getTitle = () => {
+  const elementTitle = document.querySelector('.title');
+  return elementTitle.textContent;
+};
+
+export const getDescription = () => {
+  const elemenDescr = document.querySelector('.about');
+  return elemenDescr.innerText;
+};
+
+export const getPlans = () => {
+  const elemenPlans = document.querySelector('.plans');
+  return elemenPlans.innerHTML;
+};
+
+export const getGoal = () => {
+  const elemenGoal = document.querySelector('.goal');
+  return elemenGoal.outerHTML;
+};
