cd172de42c9e64a183f805b69f46162ce0afd19d
ElioRoman <kolomiets.roman30@gmail.com>
2020-12-22 16:31:46 +0200

    add file

---
+++ lesson21/home-task7/index.js
@@ -0,0 +1,6 @@
+export const createButton = buttonText => {
+  const buttonElem = document.createElement('button');
+  buttonElem.textContent = buttonText;
+  const listElem = document.querySelector('body');
+  listElem.append(buttonElem);
+};
