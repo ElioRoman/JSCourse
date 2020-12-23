cf373c9af54e4ae5df9c099f27316e70818a1589
ElioRoman <kolomiets.roman30@gmail.com>
2020-12-23 16:28:07 +0200

    add file

---
+++ lesson22/task9/index.js
@@ -0,0 +1,5 @@
+const taskStatus = document.querySelector('.task-status');
+const handleClick = event => {
+  console.log(event.target.checked);
+};
+taskStatus.addEventListener('change', handleClick);
