21261b47bfafa15747b4126a33951b055592266a
ElioRoman <kolomiets.roman30@gmail.com>
2020-12-23 15:46:04 +0200

    add file

---
+++ lesson22/task5/index.js
@@ -0,0 +1,6 @@
+const elemSearch = document.querySelector('.search__input');
+const elemBtn = document.querySelector('.search__btn');
+const getText = () => {
+  console.log(elemSearch.value);
+};
+elemBtn.addEventListener('click', getText);
