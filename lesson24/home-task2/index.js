427914ef3f0eff4eda0179c1f8b4a81a7b163e03
ElioRoman <kolomiets.roman30@gmail.com>
2020-12-27 12:22:17 +0200

    fix

--- lesson24/home-task2/index.js
+++ lesson24/home-task2/index.js
@@ -1,15 +1,18 @@
 const students = [
   { name: 'Ben', birthDate: '01/17/2008' },
-  { name: 'Tom', birthDate: '03/15/2010' },
-  { name: 'Sam', birthDate: '03/15/2010' },
+  { name: 'Tom', birthDate: '04/15/2010' },
+  { name: 'Sam', birthDate: '04/16/2010' },
+  { name: 'Bob', birthDate: '02/13/2010' },
 ];
 
 const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Dec'];
 
-export const studentsBirthDays = () => {
+const studentsBirthDays = () => {
   const sortedStudents = [...students].sort(
-    (a, b) => new Date(a.birthDate).getDate() - new Date(b.birthDate).getDate(),
+    (a, b) => Date.parse(b.birthDate) - Date.parse(a.birthDate),
   );
+  console.log(sortedStudents);
+  console.log(students);
   const result = sortedStudents.reduce((acc, { name, birthDate }) => {
     const nameOfMonth = months[new Date(birthDate).getMonth()];
     return { ...acc, [nameOfMonth]: acc[nameOfMonth] ? acc[nameOfMonth].concat(name) : [name] };
