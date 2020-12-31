f31214af334613ad659bb8a7abb00012bf2e6727
ElioRoman <kolomiets.roman30@gmail.com>
2020-12-31 15:05:36 +0200

    fix

--- lesson33/task1/index.js
+++ lesson33/task1/index.js
@@ -3,20 +3,17 @@ const userNameElem = document.querySelector('.user__name');
 const userLocationElem = document.querySelector('.user__location');
 
 const fetchUserData = userName =>
-  fetch(`http://api.github.com/users/${userName}`).then(response => response.json());
+  fetch(`https://api.github.com/users/${userName}`).then(responce => responce.json());
 
 const renderUserData = userData => {
-  // eslint-disable-next-line camelcase
   const { avatar_url, name, location } = userData;
-
-  // eslint-disable-next-line camelcase
   userAvatarElem.src = avatar_url;
   userNameElem.textContent = name;
   userLocationElem.textContent = location ? `from ${location}` : '';
 };
 
-const userNameInputElem = document.querySelector('.name-form__input');
 const showUserBtnElem = document.querySelector('.name-form__btn');
+const userNameInputElem = document.querySelector('.name-form__input');
 
 const onSearchUser = () => {
   const userName = userNameInputElem.value;
